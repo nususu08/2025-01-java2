2ac6828a1551279470ac4cc18496b1a8081c24eb
nususu1004 <nususu1004@gmail.com>
2025-06-20 18:02:43 +0900

    20250620_IOC, DO, 인터페이스, 익명개체

---
+++ src/control/Tv.java
@@ -0,0 +1,6 @@
+package control;
+
+// is-a, has-a
+public interface Tv {
+    void sound();
+}
---
+++ src/control/legacy/HarmanSpeaker.java
@@ -0,0 +1,23 @@
+package control.legacy;
+
+import control.GenelecWoofer;
+import control.Speaker;
+import control.Woofer;
+
+public class HarmanSpeaker implements Speaker {
+    private Woofer woofer;
+
+    public HarmanSpeaker() {
+        this.woofer = new GenelecWoofer();
+    }
+
+    // HarmanSpeaker가 객체화 된다면 GenelecWoofer를 사용할 수 있도록
+    // 해주세요
+    // HarmanSpeaker speaker = new HarmanSpeaker();
+
+    @Override
+    public void speakerSound() {
+        System.out.println("HarmanSpeaker: 소리가 맑아요");
+        woofer.baseSound();
+    }
+}
